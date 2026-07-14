3e1fcea6f28a221d84c570a32a7980cc0204b4da
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-14 18:42:37 +0530

    Query params

--- Route_params/src/index.mjs
+++ Route_params/src/index.mjs
@@ -12,19 +12,29 @@ const users = [
     {id:5,user_name:"Kishan"},
 ]
 
-// app.get("/",(req,res)=>{
-//     res.send({mes:Root});
-// });
+app.get("/",(req,res)=>{
+    res.send({msg:"Root"});
+});
 
 app.get("/api/users",(req,res)=>{
     res.send(users)
 })
 
 app.get("/api/users/:id",(req,res)=>{
+    
     const id = parseInt(req.params.id);
+
     if(isNaN(id)){
         res.send({mes:"Bad Request , Invalid"})
     }
+
+    const user = users.find((user)=>user.id===id);
+
+    if(user){
+        return res.send(user);
+    }
+
+    return res.status(404).send({msg: "User not found"});
 })
 app.listen(PORT,()=>{
     console.log(`App is running on the port ${PORT}`);
