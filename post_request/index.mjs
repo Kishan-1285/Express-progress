473ca96582465755890538fc442027f11a7dacaa
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-17 23:23:41 +0530

    post request

---
+++ post_request/index.mjs
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+
+const users = [
+    {id:1,name:"Ashwin"},
+    {id:2,name:"Kishan"},
+    {id:3,name:"Rahul"}
+];
+
+const devices = [
+{ id:1, category:"mobile", brand:"Apple"},
+{ id:2, category:"mobile", brand:"Samsung"},
+{ id:3, category:"laptop", brand:"Dell"},
+{ id:4, category:"mobile", brand:"Apple"},
+{ id:5, category:"laptop", brand:"Dell"}
+];
+
+app.get('/users',(req,res)=>{
+    // const {name} = req.query;
+    // const user = users.find(user=>user.name===name);
+    //  res.json(user);
+    res.send(users);
+})
+
+app.get('/devices',(req,res)=>{
+    const {category,brand} = req.query;
+    const result = devices.filter(dev=>{
+        return dev.category===category|| dev.brand===brand;
+    })
+
+    res.json(result)
+})
+
+app.listen(3000,()=>{
+    console.log("App is running on port 3000")
+})
