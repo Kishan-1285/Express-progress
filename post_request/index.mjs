cf38d7669c85c56250ab893a2b30cba099a7ca88
kishan-1285 <kishanragunathan26@gmail.com>
2026-07-20 15:11:24 +0530

    post request

--- post_request/index.mjs
+++ post_request/index.mjs
@@ -16,25 +16,57 @@ const devices = [
 { id:5, category:"laptop", brand:"Dell"}
 ];
 
-app.get('/users',(req,res)=>{
-    // const {name} = req.query;
-    // const user = users.find(user=>user.name===name);
-    //  res.json(user);
-    // res.send(users);
+app.use(express.json());
+
+app.post('/users',(req,res)=>{
     console.log(req.body);
     const {body} = req;
-    return res.send(req.body);
+    const newuser = {id:users[users.length-1].id+1,...body};
+    users.push(newuser);    
+    return res.status(201).send(newuser);
 })
+
+app.get('/users',(req,res)=>{
+    res.send(users);
+})
+
+const cred = [
+    {id:1,username:"kishan", password:"asdf"}
+];
+
+app.post("/login", (req, res) => {
+
+    // const username = req.body.username;
+    // const password = req.body.password;
+   
+//      -------0r-------
+ 
+    const{username,password} = req.body;
+    const {body} = req;
+     const id = cred.length === 0
+        ? 1
+        : cred[cred.length - 1].id + 1;
+
+    const newUser = {
+        id,
+        username,
+        password
+    };
+    cred.push(newUser);
+    res.send(newUser);
+
+    // res.send("Login Request Received");
+
+});
 
 app.get('/devices',(req,res)=>{
     const {category,brand} = req.query;
     const result = devices.filter(dev=>{
         return dev.category===category|| dev.brand===brand;
     })
-
     res.json(result)
 })
 
 app.listen(3000,()=>{
-    console.log("App is running on port 3000")
+    console.log("App is running on port 3000");
 })
